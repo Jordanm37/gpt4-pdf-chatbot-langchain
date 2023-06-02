16ad101e501f99ec5c861baa85920010b45d9c5c
Jordanm37 <64754031+Jordanm37@users.noreply.github.com>
2023-06-03 08:34:02 +1000

    update docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
 import { Document } from 'langchain/document';
+import { InlineMath, BlockMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
 import {
   Accordion,
   AccordionContent,
@@ -25,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Hi, what would you like to learn about?',
         type: 'apiMessage',
       },
     ],
@@ -40,6 +42,13 @@ export default function Home() {
   useEffect(() => {
     textAreaRef.current?.focus();
   }, []);
+
+  const renderers = {
+    inlineMath: ({ value }: { value: string }) => (
+      <InlineMath math={value} />
+    ),
+    math: ({ value }: { value: string }) => <BlockMath math={value} />,
+  };
 
   //handle form submission
   async function handleSubmit(e: any) {
@@ -125,7 +134,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chat With Your Docs
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -169,7 +178,7 @@ export default function Home() {
                       <div key={`chatMessage-${index}`} className={className}>
                         {icon}
                         <div className={styles.markdownanswer}>
-                          <ReactMarkdown linkTarget="_blank">
+                          <ReactMarkdown components={renderers} linkTarget="_blank">
                             {message.message}
                           </ReactMarkdown>
                         </div>
@@ -218,13 +227,13 @@ export default function Home() {
                     ref={textAreaRef}
                     autoFocus={false}
                     rows={1}
-                    maxLength={512}
+                    maxLength={3000}
                     id="userInput"
                     name="userInput"
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What is your question?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -260,11 +269,11 @@ export default function Home() {
             )}
           </main>
         </div>
-        <footer className="m-auto p-4">
+        {/* <footer className="m-auto p-4">
           <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+            Powered by LangChainAI. 
           </a>
-        </footer>
+        </footer> */}
       </Layout>
     </>
   );
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,19 +9,17 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `You will act as an AI Assistant that I am having a conversation with. You have the expertise of an expert physics professor that specialises in advanced mathematical physics and dynamical mechanics. You will provide answers and guidance from your extensive knowledge, and will always provide relevant theorems when requested. Additionally, you will ask follow-up questions to clarify the last response and provide more accurate and personalized answers. If the answer is not included in your knowledge base, you will say "Hmm, I am not sure." and stop after that. Your goal is to provide the best possible guidance and support to help me with my queries and problems. You think things through step by step every time. You break down problems into simple steps before solving and always double check your answers. 
 
 {context}
 
 Question: {question}
-Helpful answer in markdown:`;
+Helpful answer in markdown or latex where equations are included:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    temperature: 0.7, // increase temepreature to get more creative answers
+    modelName: 'gpt-4', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
